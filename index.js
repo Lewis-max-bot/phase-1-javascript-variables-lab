0d1d08a506dd22aa5c6421e462a19979acbd5ff1
Lewis <lewis.njuma@student.moringaschool.com>
2025-03-06 23:36:56 +0300

    Using variables

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+// let companyName = 'Scuber';
+// const companyName = 'Scuber';
+// let mostProfitableNeighborhood = 'Chelsea';
+// let companyCeo = 'Susan Smith';
+// let companyName = 'Scuber';
+// const mostProfitableNeighborhood = 'Chelsea';
+const companyCeo = 'Susan Smith';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyName = 'Scuber';
+
+
